fd3bdd4c2b9cbd821071b20b0118157f257c7215
Patrick Lai <patrick.lai@florey.com.au>
2018-03-16 13:33:03 +1100

    added note to wait for heroku

--- src/screens/NLAI/index.js
+++ src/screens/NLAI/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return   <div className="page padded" style={{ display: "flex", flexDirection: 'column'}}>
-    <p>This has stopped working as my Watson.ai trial period expired</p>
+    <p>Please wait a bit for heroku to spin up. <i>Note - This has stopped working as my Watson.ai trial period expired</i></p>
     <iframe src="https://lululeague-assistant.herokuapp.com/"  style={{ width: '100%', flex: 1}} frameBorder={0} />
   </div>;
 }
