148623b59aed598f129b922ff76533ce1a5cc06f
Patrick Lai <patrick.lai@florey.com.au>
2018-03-16 23:59:15 +1100

    Added my email

--- src/App/index.js
+++ src/App/index.js
@@ -9,11 +9,24 @@ import { fade } from "react-app-engine/src/transitions";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
+const NavContent = (
+  <div>
+    <h3>Patrick Lai</h3>
+    <h6>Technical Lead</h6>
+    <h6>Full stack javascript</h6>
+    <h6>
+      <a style={{ color: "white" }} href="mailto:mrphlai@gmail.com">
+        mrphlai@gmail.com
+      </a>
+    </h6>
+  </div>
+);
+
 export default class App extends React.Component {
   render() {
     return (
       <div className={styles.app}>
-        <SideNav items={tabs} />
+        <SideNav items={tabs} content={NavContent} />
         <div className="page">
           <AnimatedSwitch {...fade} className="switch-wrapper">
             {tabs.map((tab, index) => <Route key={index} {...tab} />)}
